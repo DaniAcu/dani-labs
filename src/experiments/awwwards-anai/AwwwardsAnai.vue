452773300d11faf26792c3c2e3811ba53362d5fa
Daniel <daniel.acuna2@greatminds.org>
2024-05-27 18:20:08 -0300

    feat: add smooth scroll

--- src/experiments/awwwards-anai/AwwwardsAnai.vue
+++ src/experiments/awwwards-anai/AwwwardsAnai.vue
@@ -9,6 +9,7 @@ import { onMounted } from 'vue'
 
 import gsap from 'gsap';
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import ScrollSmoother from "gsap/dist/ScrollSmoother";
 
 import Grid from './components/Grid.vue';
 import { ImageManager } from '../../images/ImageManager';
@@ -17,7 +18,8 @@ import { ImageManager } from '../../images/ImageManager';
 const headerImg = ImageManager.get("EOIToTneyZ4", "high");
 
 onMounted(() => {
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  ScrollSmoother.create({ smooth: 1, effects: true, smoothTouch: 0.1 });
   gsap.to(".image-frame", {
     "--horizontal": 0,
     "--vertical": 0,
